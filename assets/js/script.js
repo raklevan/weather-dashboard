5b6a4069ddb94fba2d541ff51c85cb5978f821ff
Larry <richardklevan@gmail.com>
2019-10-31 11:05:52 -0700

    updating files

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var currentWeatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?&a
 
 var currentUviQueryURL= "https://api.openweathermap.org/data/2.5/uvi?&appid="+key
 
+var forecastWeatherQueryURL= "https://api.openweathermap.org/data/2.5/forecast?appid=" +key+"&units=imperial"
 
 
 
@@ -14,10 +15,21 @@ $(".button").click(function(event){
 
     var cityName = $("#search-bar").val();
 
-    console.log(cityName)
+    runCity(cityName);
+
+    forecast(cityName);
+
+})
+
+
+
+
+function runCity(city){
+
+    console.log(city)
 
     $.ajax({
-        url: currentWeatherQueryURL + "&q="+ cityName,
+        url: currentWeatherQueryURL + "&q="+ city,
         method: "GET"
 
     }).then(function(response){
@@ -29,7 +41,6 @@ $(".button").click(function(event){
 
          $("#current-windspeed").text(" "+response.wind.speed)
 
-
          $.ajax({
 
             url: currentUviQueryURL+"&lat="+response.coord.lat+"&lon="+response.coord.lon,
@@ -40,15 +51,29 @@ $(".button").click(function(event){
             $("#current-uvi").text(" "+response.value)
 
          })
-
         //  $("#current-temperature").text(" "+response.main.temp)
 
+    })
+}
+
+
+function forecast(city){
+    $.ajax({
+        url: forecastWeatherQueryURL+ "&q="+ city,
+        method: "GET"
+    }).then(function(response){
+
+        for (let i = 0; i < 5; i++) {
+            $("#forecast-column").append("<div class='card'> <div class='card-body'> <h5 class='card-title'>"+response.list[8*i].dt_txt  +"</h5> temperature: "+ response.list[8*i].main.temp+
+            "<br> humidity: " + response.list[8*i].main.humidity +
+            "<br> wind speed: "+ response.list[8*i].wind.speed+"</div></div>")
+
+
+            
+        }
 
 
 
 
     })
-
-
-
-})
+}
